7b1feb1b4fb04e8a1c2e55e9ab8fcce131881132
ditam <ditam@users.noreply.github.com>
2025-10-06 01:25:46 +0200

    add basic upgrade dialog with effects

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 const WIDTH = 800;
 const HEIGHT = 500;
-const PLAYER_SPEED = 4;
 const PLAYER_SIZE = 20;
 const ACTIVITY_RADIUS = 50;
 // movement within this margin attempts to scroll the viewport if possible
@@ -9,6 +8,15 @@ const VIEWPORT_SCROLL_MARGIN = 150;
 
 const forestBGColor = 'rgb(50, 100, 50)';
 const townBGColor = 'rgba(93, 83, 49, 1)';
+
+// upgradeable properties
+let INVENTORY_SIZE = 1;
+let PLAYER_SPEED = 1;
+let VISION_SIZE = 1;
+let TOOL_STRENGTH = 1;
+let hasFollower = false;
+
+const inventory = [];
 
 // The game starts in town
 let inTown = true;
@@ -33,6 +41,7 @@ function getRandomInt(min, max) { // min and max included
 const player_internal = {
   x: 300,
   y: 120,
+  score: 0,
 };
 
 const viewport = {
@@ -40,7 +49,7 @@ const viewport = {
   // TODO: guards against negatives
   x: 0,
   y: 0,
-}
+};
 let player = new Proxy(player_internal, {
   set(target, prop, value) {
     // TODO: adjust viewport x/y
@@ -48,6 +57,9 @@ let player = new Proxy(player_internal, {
       target.x = value;
     } else if (prop === 'y') {
       target.y = value;
+    } else if (prop === 'score') {
+      target.score = value;
+      // TODO: update counter
     } else {
       console.warn('Unknown property setter:', prop);
       return Reflect.set(...arguments);
@@ -82,8 +94,6 @@ function generateTownObjects() {
 }
 generateTownObjects()
 
-const inventory = [];
-
 const trees = [];
 for (let i=0; i<20; i++) {
   for (let j=0; j<12; j++) {
@@ -98,8 +108,85 @@ function dist(a, b) {
   return Math.sqrt(dX*dX + dY*dY);
 }
 
+let merchantScreen;
+function getUpgradePrice(name, level) {
+  console.assert(['inventory', 'speed', 'vision', 'tool'].includes(name));
+  console.assert(Number.isInteger(level) && level >= 0);
+  if (level > 4) {
+    return 9999;
+  }
+  return {
+    'inventory': [0, 1, 2, 3, 4],
+    'speed':     [0, 1, 2, 3, 4],
+    'vision':    [0, 1, 2, 3, 4],
+    'tool':      [0, 1, 2, 3, 4]
+  }[name][level];
+}
+function initMerchantDialog() {
+  merchantScreen = $('#merchant-screen');
+  merchantScreen.hide();
+  merchantScreen.find('#p1').text('Welcome, Stranger!');
+  merchantScreen.find('#p2').text('Are you here for the mushroom season?');
+
+  merchantScreen.find('#hireling-upgrade').hide();
+
+  // TODO: display upg prices
+  merchantScreen.find('#inventory-upgrade .button').click(e => {
+    const upgCost = getUpgradePrice('inventory', INVENTORY_SIZE+1);
+    if (player.score < upgCost) {
+      console.log('Cant afford inventory upgrade:', upgCost);
+      // FIXME: play error sound
+    } else {
+      player.score -= upgCost;
+      INVENTORY_SIZE++;
+    }
+  });
+  merchantScreen.find('#speed-upgrade .button').click(e => {
+    const upgCost = getUpgradePrice('speed', PLAYER_SPEED+1);
+    if (player.score < upgCost) {
+      console.log('Cant afford speed upgrade:', upgCost);
+      // FIXME: play error sound
+    } else {
+      player.score -= upgCost;
+      PLAYER_SPEED++;
+    }
+  });
+  merchantScreen.find('#vision-upgrade .button').click(e => {
+    const upgCost = getUpgradePrice('vision', VISION_SIZE+1);
+    if (player.score < upgCost) {
+      console.log('Cant afford vision upgrade:', upgCost);
+      // FIXME: play error sound
+    } else {
+      player.score -= upgCost;
+      VISION_SIZE++;
+    }
+  });
+  merchantScreen.find('#tool-upgrade .button').click(e => {
+    const upgCost = getUpgradePrice('tool', TOOL_STRENGTH+1);
+    if (player.score < upgCost) {
+      console.log('Cant afford tool upgrade:', upgCost);
+      // FIXME: play error sound
+    } else {
+      player.score -= upgCost;
+      TOOL_STRENGTH++;
+    }
+  });
+  merchantScreen.find('#hireling-upgrade .button').click(e => {
+    const upgCost = 5000;
+    if (player.score < upgCost) {
+      console.log('Cant afford hireling upgrade:', upgCost);
+      // FIXME: play error sound
+    } else {
+      player.score -= upgCost;
+      hasFollower = true;
+    }
+  });
+
+  // TODO: upgrade button to show next level price
+};
+
 function showMerchantDialog() {
-  console.log('opening merchant dialog...');
+  merchantScreen.show();
 }
 
 function interact() {
@@ -116,9 +203,19 @@ function interact() {
         }
       } else {
         // in forest: remove item from map and move to inventory
+        if (o.type === 'large' && TOOL_STRENGTH < 2) {
+          // FIXME: show error
+          // FIXME: play error sound
+          // TODO: tool size should have effect beyond 2 as well - increased item values maybe?
+          return;
+        }
         const item = mapObjects.splice(i, 1)[0];
         console.log('found item:', item);
         inventory.push({type: item.type});
+        if (inventory.length >= (INVENTORY_SIZE+5)) {
+          // TODO: somehow explain to user why they go back? Simple confirm message?
+          switchScenes();
+        }
       }
       return true;
     }
@@ -158,30 +255,31 @@ function switchScenes() {
 }
 
 function applyMovement() {
+  const effectiveSpeed = PLAYER_SPEED + 3;
   const xInViewPort = player.x - viewport.x;
   const yInViewPort = player.y - viewport.y;
   if (keysPressed.up && player.y > 0) {
-    player.y = Math.max(0, player.y - PLAYER_SPEED);
+    player.y = Math.max(0, player.y - effectiveSpeed);
     if (yInViewPort <= VIEWPORT_SCROLL_MARGIN) {
-      viewport.y = Math.max(0, viewport.y - PLAYER_SPEED);
+      viewport.y = Math.max(0, viewport.y - effectiveSpeed);
     }
   }
   if (keysPressed.right && player.x < MAP_WIDTH) {
-    player.x = Math.min(MAP_WIDTH, player.x + PLAYER_SPEED);
+    player.x = Math.min(MAP_WIDTH, player.x + effectiveSpeed);
     if (WIDTH - xInViewPort <= VIEWPORT_SCROLL_MARGIN) {
-      viewport.x = Math.min(MAP_WIDTH - WIDTH, viewport.x + PLAYER_SPEED);
+      viewport.x = Math.min(MAP_WIDTH - WIDTH, viewport.x + effectiveSpeed);
     }
   }
   if (keysPressed.down && player.y < MAP_HEIGHT) {
-    player.y = Math.min(MAP_HEIGHT, player.y + PLAYER_SPEED);
+    player.y = Math.min(MAP_HEIGHT, player.y + effectiveSpeed);
     if (HEIGHT - yInViewPort <= VIEWPORT_SCROLL_MARGIN) {
-      viewport.y = Math.min(MAP_HEIGHT - HEIGHT, viewport.y + PLAYER_SPEED);
+      viewport.y = Math.min(MAP_HEIGHT - HEIGHT, viewport.y + effectiveSpeed);
     }
   }
   if (keysPressed.left && player.x > 0) {
-    player.x = Math.max(0, player.x - PLAYER_SPEED);
+    player.x = Math.max(0, player.x - effectiveSpeed);
     if (xInViewPort <= VIEWPORT_SCROLL_MARGIN) {
-      viewport.x = Math.max(0, viewport.x - PLAYER_SPEED);
+      viewport.x = Math.max(0, viewport.x - effectiveSpeed);
     }
   }
 }
@@ -231,7 +329,7 @@ function drawFrame(timestamp) {
   ctx2.filter = 'blur(20px)';
   ctx2.globalCompositeOperation = 'destination-out';
   ctx2.beginPath();
-  ctx2.arc(xInViewPort, yInViewPort, 120, 0, Math.PI*2);
+  ctx2.arc(xInViewPort, yInViewPort, (VISION_SIZE+3)*20, 0, Math.PI*2);
   ctx2.fill();
   ctx2.restore();
 
@@ -258,6 +356,8 @@ $(document).ready(function() {
   ctx.fillStyle = '#2194caff';
   ctx.strokeStyle = 'green';
   ctx.lineWidth = 1;
+
+  initMerchantDialog();
 
   document.addEventListener('keydown', event => {
     switch(event.code) {
